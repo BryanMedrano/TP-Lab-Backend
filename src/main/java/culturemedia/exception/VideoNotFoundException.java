3d89ce60e4aa41980e8faba7f4d769bbee835cb7
BryanMedrano <medranoortegabryan@gmail.com>
2024-10-16 20:37:43 -0500

    modifying videoNotFoundException

--- src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -3,6 +3,9 @@ package culturemedia.exception;
 import java.text.MessageFormat;
 
 public class VideoNotFoundException extends CulturotecaException{
+    public VideoNotFoundException(){
+        super("Video not found");
+    }
     public VideoNotFoundException(String title){
         super(MessageFormat.format("Video not found by title {0}",title));
     }
