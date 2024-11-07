56858740d78cadd2fda62cdf2ae06efc50630bb1
BryanMedrano <medranoortegabryan@gmail.com>
2024-11-07 08:46:04 -0500

    Adding Components Rest API. And some changes after class review

--- src/test/java/culturemedia/service/CulturotecaServiceTest.java
+++ src/test/java/culturemedia/service/CulturotecaServiceTest.java
@@ -1,4 +1,4 @@
-package culturemedia.service.impl;
+package culturemedia.service;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
@@ -6,20 +6,19 @@ import culturemedia.repository.VideoRepository;
 import culturemedia.repository.ViewsRepository;
 import culturemedia.repository.impl.VideoRepositoryImpl;
 import culturemedia.repository.impl.ViewsRepositoryImpl;
-import culturemedia.service.CulturotecaService;
+import culturemedia.service.impl.CulturotecaServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 
 
-class CulturotecaServiceImplTest {
+
+class CulturotecaServiceTest {
 
     private CulturotecaService culturotecaService;
     @Mock
@@ -34,32 +33,21 @@ class CulturotecaServiceImplTest {
 
     @Test
     void when_FindAll_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully()  {
-        doReturn(List.of()).when(videoRepository).findAll();
+        mockFindAllDoesNotFind();
         culturotecaService = new CulturotecaServiceImpl( new VideoRepositoryImpl(), new ViewsRepositoryImpl());
-        VideoNotFoundException exception = assertThrows(VideoNotFoundException.class, () -> culturotecaService.findAll());
+        assertThrows(VideoNotFoundException.class, () -> culturotecaService.findAll());
     }
 
     @Test
     void when_FindAll_all_videos_should_be_returned_successfully() throws VideoNotFoundException{
-        doReturn(List.of(
-                new Video("01", "Título 1", "Descripción 1", 4.5),
-                new Video("02", "Título 2", "Descripción 2", 5.5),
-                new Video("03", "Título 3", "Descripción 3", 4.4),
-                new Video("04", "Título 4", "Descripción 4", 3.5),
-                new Video("05", "Clic 5", "Descripción 5", 5.7),
-                new Video("06", "Clic 6", "Descripción 6", 5.1)
-        )).when(videoRepository).findAll();
+        mockFindAllReturnsVideos();
         List<Video> videos = culturotecaService.findAll();
         assertEquals(6, videos.size());
     }
 
     @Test
     void when_FindByTitle_videos_with_the_title_should_be_returned_successfully() throws VideoNotFoundException {
-        doReturn(List.of(
-                new Video("05", "Clic 5", "Descripción 5", 5.7),
-                new Video("06", "Clic 6", "Descripción 6", 5.1)
-        )).when(videoRepository).findAll();
-
+        mockFindByTitleReturnsVideos();
         List<Video> videos = culturotecaService.findByTitle("Clic");
         assertEquals(2, videos.size());
         assertEquals("Clic 5", videos.get(0).title());
@@ -68,27 +56,59 @@ class CulturotecaServiceImplTest {
 
     @Test
     void when_FindByTitle_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
-        doReturn(List.of()).when(videoRepository).find("Laboratory");
+        mockFindByTitleDoesNotFind("Laboratory");
         VideoNotFoundException exception = assertThrows(VideoNotFoundException.class, () -> culturotecaService.findByTitle("Laboratory"));
         assertEquals("Video not found by title: Laboratory", exception.getMessage());
     }
 
     @Test
     void when_FindByDuration_videos_in_the_duration_range_should_be_returned_successfully() throws VideoNotFoundException {
-        doReturn(List.of(
-                new Video("01", "Título 1", "Descripción 1", 4.5),
-                new Video("02", "Título 2", "Descripción 2", 5.5),
-                new Video("06", "Clic 6", "Descripción 3", 5.1)
-        )).when(videoRepository).findAll();
+        mockFindByDurationReturnsVideos();
         List<Video> videos = culturotecaService.findByDuration(4.5, 5.5);
         assertEquals(3, videos.size());
     }
 
     @Test
     void when_FindByDuration_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
-        doReturn(List.of()).when(videoRepository).find(1.0, 2.0);
+        mockFindByDurationDoesNotFind();
         VideoNotFoundException exception = assertThrows(VideoNotFoundException.class, () -> culturotecaService.findByDuration(1.0, 2.0));
         assertEquals("Video not found.", exception.getMessage());
     }
 
+    private void mockFindAllReturnsVideos(){
+        doReturn(List.of(new Video("01", "Título 1", "Descripción 1", 4.5),
+                new Video("02", "Título 2", "Descripción 2", 5.5),
+                new Video("03", "Título 3", "Descripción 3", 4.4),
+                new Video("04", "Título 4", "Descripción 4", 3.5),
+                new Video("05", "Clic 5", "Descripción 5", 5.7),
+                new Video("06", "Clic 6", "Descripción 6", 5.1))
+        ).when(videoRepository).findAll();
+    }
+
+    private void mockFindAllDoesNotFind() {
+        doReturn(List.of()).when(videoRepository).findAll();
+    }
+
+    private void mockFindByTitleReturnsVideos() {
+        doReturn(List.of(
+                new Video("05", "Clic 5", "Descripción 5", 5.7),
+                new Video("06", "Clic 6", "Descripción 6", 5.1)
+        )).when(videoRepository).findAll();
+    }
+
+    private void mockFindByTitleDoesNotFind(String title) {
+        doReturn(List.of()).when(videoRepository).find(title);
+    }
+
+    private void mockFindByDurationReturnsVideos() {
+        doReturn(List.of(
+                new Video("01", "Título 1", "Descripción 1", 4.5),
+                new Video("02", "Título 2", "Descripción 2", 5.5),
+                new Video("06", "Clic 6", "Descripción 3", 5.1)
+        )).when(videoRepository).findAll();
+    }
+
+    private void mockFindByDurationDoesNotFind() {
+        doReturn(List.of()).when(videoRepository).find(1.0, 2.0);
+    }
 }
